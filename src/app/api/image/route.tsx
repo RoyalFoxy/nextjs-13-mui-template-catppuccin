b59f030665017fee5d73a2efbcbb2e9a858b006c
RoyalFoxy <sepp.toni.mo@gmail.com>
2023-11-07 16:54:06 +0100

    Move colors to seperate file to avoid client functions being called on the server.

--- src/app/api/image/route.tsx
+++ src/app/api/image/route.tsx
@@ -1,4 +1,4 @@
-import { latte, mocha } from "@theme/palette";
+import { latteColors as latte, mochaColors as mocha } from "@theme/colors";
 
 import { ImageResponse } from "next/og";
 import { NextRequest } from "next/server";
